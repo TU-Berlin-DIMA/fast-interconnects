22a8f9fcfe75ad564e7be99e40672632fc88b834
Clemens Lutz <lutzcle@cml.li>
2020-12-11 22:16:42 +0100

    radix-join: Tune grid and block sizes

--- radix-join/src/main.rs
+++ radix-join/src/main.rs
@@ -314,8 +314,8 @@ where
     // Device tuning
     let multiprocessors = device.get_attribute(DeviceAttribute::MultiprocessorCount)? as u32;
     let warp_size = device.get_attribute(DeviceAttribute::WarpSize)? as u32;
-    let warp_overcommit_factor = 4;
-    let grid_overcommit_factor = 2;
+    let warp_overcommit_factor = 32;
+    let grid_overcommit_factor = 1;
 
     let block_size = BlockSize::x(warp_size * warp_overcommit_factor);
     let grid_size = GridSize::x(multiprocessors * grid_overcommit_factor);
