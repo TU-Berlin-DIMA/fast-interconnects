df97ad5db593299d0785cfc68df7cc5c7224e952
Clemens Lutz <lutzcle@cml.li>
2021-05-06 18:56:13 +0200

    Switch Nix to Mozilla's Rust overlay for most recent Rust version

--- shell.nix
+++ shell.nix
@@ -1,12 +1,23 @@
 with import <nixpkgs> {};
 
+let src = fetchFromGitHub {
+      owner = "mozilla";
+      repo = "nixpkgs-mozilla";
+      # commit from: 2019-05-15
+      rev = "8c007b60731c07dd7a052cce508de3bb1ae849b4";
+      sha256 = "1zybp62zz0h077zm2zmqs2wcg3whg6jqaah9hcl1gv4x8af4zhs6";
+   };
+in
+with import "${src.out}/rust-overlay.nix" pkgs pkgs;
+
 gcc8Stdenv.mkDerivation {
   name = "rust-env";
   nativeBuildInputs = [
-    rustc rustfmt cargo cargo-flamegraph clang numactl cudatoolkit_10_2 linuxPackages.nvidia_x11 valgrind
+    # Rust overlay packages
+    latest.rustChannels.stable.rust
 
-    # Example Build-time Additional Dependencies
-    # pkgconfig
+    # NixPkgs packages
+    clang numactl cudatoolkit_10_2 linuxPackages.nvidia_x11 valgrind
   ];
   buildInputs = [
     # Example Run-time Additional Dependencies
